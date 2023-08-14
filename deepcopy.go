de92b1d68d136f8290f7c23ac33994f62ca91557
xieyuschen <xieyuschen@gmail.com>
2023-08-14 18:02:01 +0800

    enhancement: return error to exit recursive call

--- deepcopy.go
+++ deepcopy.go
@@ -1,9 +1,6 @@
-// deepcopy makes deep copies of things. A standard copy will copy the
+// Package deepcopy makes deep copies of things. A standard copy will copy the
 // pointers: deep copy copies the values pointed to.  Unexported field
 // values are not copied.
-//
-// Copyright (c)2014-2016, Joel Scoble (github.com/mohae), all rights reserved.
-// License: MIT, for more details check the included LICENSE file.
 package deepcopy
 
 import (
@@ -26,22 +23,16 @@ type Interface interface {
 	DeepCopy() interface{}
 }
 
-// Copy creates a deep copy of whatever is passed to it and returns the copy
-// in an interface{}.  The returned value will need to be asserted to the
-// correct type.
-func Copy(src interface{}) (copied interface{}, err error) {
+// Copy returns a deep copy value of the original value.
+// All the values are expected to be deep copied except some cases:
+// 1. all unexposed fields in a struct won't be deep copied
+// 2. channels are shared when deep copy
+// 3. functions are shared when deep copy, beware of variables captured by your function when you do deep copy.
+// 4. Copy will return errors for circular references and too long reference chains.
+func Copy(src interface{}) (interface{}, error) {
 	if src == nil {
 		return nil, nil
 	}
-	defer func() {
-		if r := recover(); r != nil {
-			if e, ok := r.(deepCopyError); ok {
-				err = e
-			} else {
-				err = fmt.Errorf("%s", r)
-			}
-		}
-	}()
 	// Make the interface a reflect.Value
 	original := reflect.ValueOf(src)
 	
@@ -49,28 +40,30 @@ func Copy(src interface{}) (copied interface{}, err error) {
 	cpy := reflect.New(original.Type()).Elem()
 	
 	// Recursively copy the original.
-	copyRecursive(original, cpy, &callState{
+	err := copyRecursive(original, cpy, &callState{
 		ptrLevel: 0,
 		ptrSeen:  make(map[interface{}]struct{})})
+	if err != nil {
+		return nil, err
+	}
+	
 	// Return the copy as an interface.
 	return cpy.Interface(), nil
 }
 
-// copyRecursive does the actual copying of the interface. It currently has
-// limited support for what it can handle. Add as needed.
-func copyRecursive(original, cpy reflect.Value, state *callState) {
+func copyRecursive(original, cpy reflect.Value, state *callState) error {
 	state.ptrLevel++
 	defer func() {
 		state.ptrLevel--
 	}()
 	if int(state.ptrLevel) > maxReferenceChainLength {
-		panic(deepCopyError{fmt.Errorf("excessive reference chain happened via %s", original.Type().String())})
+		return fmt.Errorf("excessive reference chain happened via %s", original.Type().String())
 	}
 	// check for implement Interface
 	if original.CanInterface() {
 		if copier, ok := original.Interface().(Interface); ok {
 			cpy.Set(reflect.ValueOf(copier.DeepCopy()))
-			return
+			return nil
 		}
 	}
 	
@@ -83,7 +76,7 @@ func copyRecursive(original, cpy reflect.Value, state *callState) {
 		// to be paid for checking.
 		if state.ptrLevel > uint(startDetectingCyclesAfter) {
 			if _, ok := state.ptrSeen[ptr]; ok {
-				panic(deepCopyError{fmt.Errorf("encountered a circular reference via %s", original.Type().String())})
+				return fmt.Errorf("encountered a circular reference via %s", original.Type().String())
 			}
 			state.ptrSeen[ptr] = struct{}{}
 			defer delete(state.ptrSeen, ptr)
@@ -92,28 +85,33 @@ func copyRecursive(original, cpy reflect.Value, state *callState) {
 		originalValue := original.Elem()
 		// if it isn't valid, return.
 		if !originalValue.IsValid() {
-			return
+			return nil
 		}
 		cpy.Set(reflect.New(originalValue.Type()))
 		
-		copyRecursive(originalValue, cpy.Elem(), state)
-	
+		err := copyRecursive(originalValue, cpy.Elem(), state)
+		if err != nil {
+			return err
+		}
 	case reflect.Interface:
 		// If this is a nil, don't do anything
 		if original.IsNil() {
-			return
+			return nil
 		}
 		// Get the value for the interface, not the pointer.
 		originalValue := original.Elem()
 		// Get the value by calling Elem().
 		copyValue := reflect.New(originalValue.Type()).Elem()
-		copyRecursive(originalValue, copyValue, state)
+		err := copyRecursive(originalValue, copyValue, state)
+		if err != nil {
+			return err
+		}
 		cpy.Set(copyValue)
 	case reflect.Struct:
 		t, ok := original.Interface().(time.Time)
 		if ok {
 			cpy.Set(reflect.ValueOf(t))
-			return
+			return nil
 		}
 		// Go through each field of the struct and copy it.
 		for i := 0; i < original.NumField(); i++ {
@@ -123,7 +121,10 @@ func copyRecursive(original, cpy reflect.Value, state *callState) {
 			if original.Type().Field(i).PkgPath != "" {
 				continue
 			}
-			copyRecursive(original.Field(i), cpy.Field(i), state)
+			err := copyRecursive(original.Field(i), cpy.Field(i), state)
+			if err != nil {
+				return err
+			}
 		}
 	
 	case reflect.Slice:
@@ -140,61 +141,73 @@ func copyRecursive(original, cpy reflect.Value, state *callState) {
 			// The length is stored to distinguish the slice has been seen before
 			// correctly to avoid cases like right fold a slice.
 			ptr := struct {
-				ptr unsafe.Pointer
-				len int
+				ptr    unsafe.Pointer
+				length int
 			}{unsafe.Pointer(original.Pointer()), original.Len()}
 			
 			if _, ok := state.ptrSeen[ptr]; ok {
-				panic(deepCopyError{fmt.Errorf("encountered a circular reference via %s", original.Type().String())})
+				return fmt.Errorf("encountered a circular reference via %s", original.Type().String())
 			}
 			state.ptrSeen[ptr] = struct{}{}
 			defer delete(state.ptrSeen, ptr)
 		}
 		
 		if original.IsNil() {
-			return
+			return nil
 		}
 		// Make a new slice and copy each element.
 		cpy.Set(reflect.MakeSlice(original.Type(), original.Len(), original.Cap()))
 		for i := 0; i < original.Len(); i++ {
-			copyRecursive(original.Index(i), cpy.Index(i), state)
+			err := copyRecursive(original.Index(i), cpy.Index(i), state)
+			if err != nil {
+				return err
+			}
 		}
 	case reflect.Array:
 		// since origin is an array, the capacity of array will be conserved
 		cpy.Set(reflect.New(original.Type()).Elem())
 		for i := 0; i < original.Len(); i++ {
-			copyRecursive(original.Index(i), cpy.Index(i), state)
+			err := copyRecursive(original.Index(i), cpy.Index(i), state)
+			if err != nil {
+				return err
+			}
 		}
 	case reflect.Map:
 		ptr := unsafe.Pointer(original.Pointer())
 		if state.ptrLevel > uint(startDetectingCyclesAfter) {
 			if _, ok := state.ptrSeen[ptr]; ok {
-				panic(deepCopyError{fmt.Errorf("encountered a circular reference via %s", original.Type().String())})
+				return fmt.Errorf("encountered a circular reference via %s", original.Type().String())
 			}
 			state.ptrSeen[ptr] = struct{}{}
 			defer delete(state.ptrSeen, ptr)
 		}
 		
 		if original.IsNil() {
-			return
+			return nil
 		}
 		cpy.Set(reflect.MakeMap(original.Type()))
 		for _, key := range original.MapKeys() {
 			originalValue := original.MapIndex(key)
 			copyValue := reflect.New(originalValue.Type()).Elem()
-			copyRecursive(originalValue, copyValue, state)
+			err := copyRecursive(originalValue, copyValue, state)
+			if err != nil {
+				return err
+			}
 			copiedKey := reflect.New(key.Type()).Elem()
-			copyRecursive(key, copiedKey, state)
+			err = copyRecursive(key, copiedKey, state)
+			if err != nil {
+				return err
+			}
+			
 			cpy.SetMapIndex(copiedKey, copyValue)
 		}
 	default:
 		cpy.Set(original)
 	}
+	return nil
 }
 
 type callState struct {
 	ptrLevel uint
 	ptrSeen  map[interface{}]struct{}
 }
-
-type deepCopyError struct{ error }
